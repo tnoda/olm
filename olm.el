8ad3dce2849c7d3601e601ecb8dda5f76db51a1a
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-08-06 17:47:15 +0900

    Add headers.

--- olm.el
+++ olm.el
@@ -1,5 +1,31 @@
-(eval-when-compile
-  (require 'dash))
+;;; olm.el --- Outlook Mail in Emacs
+
+;; Copyright (C) 2014  Takahiro Noda
+
+;; Author: Takahiro Noda <takahiro.noda+github@gmail.com>
+;; Keywords: mail
+;; Package-Requires: ((dash "2.8.0))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Outlook Mail in Emacs
+
+;;; Code:
+
+(require 'dash)
 
 (defvar olm-folder-id nil)
 (defvar olm-folder-name nil)
@@ -496,3 +522,9 @@
   (olm-draft-kill))
 
 (add-hook 'olm-draft-reply-all-hook 'olm-message-mode-keyword)
+
+
+
+(provide 'olm)
+;;; olm.el ends here
+
