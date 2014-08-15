25f84f5fdf740c5c6c2051dd05670c01bdd96470
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-08-15 22:08:13 +0900

    Fix an unbalanced double quotation.

--- olm.el
+++ olm.el
@@ -4,7 +4,7 @@
 
 ;; Author: Takahiro Noda <takahiro.noda+github@gmail.com>
 ;; Keywords: mail
-;; Package-Requires: ((dash "2.8.0))
+;; Package-Requires: ((dash "2.8.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
